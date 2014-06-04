003ba14fab258804872d37faac088acacffb5020
bend <bend0303@gmail.com>
2014-06-04 11:03:21 +0300

    Faceboog login

--- config/env/development.js
+++ config/env/development.js
@@ -1,28 +1,28 @@
 'use strict';
 
 module.exports = {
-	db: 'mongodb://localhost/audiofind-dev',
-	app: {
-		title: 'AudioFind - Development Environment'
-	},
-	facebook: {
-		clientID: process.env.FACEBOOK_ID || 'APP_ID',
-		clientSecret: process.env.FACEBOOK_SECRET || 'APP_SECRET',
-		callbackURL: 'http://localhost:3000/auth/facebook/callback'
-	},
-	twitter: {
-		clientID: process.env.TWITTER_KEY || 'CONSUMER_KEY',
-		clientSecret: process.env.TWITTER_SECRET || 'CONSUMER_SECRET',
-		callbackURL: 'http://localhost:3000/auth/twitter/callback'
-	},
-	google: {
-		clientID: process.env.GOOGLE_ID || 'APP_ID',
-		clientSecret: process.env.GOOGLE_SECRET || 'APP_SECRET',
-		callbackURL: 'http://localhost:3000/auth/google/callback'
-	},
-	linkedin: {
-		clientID: process.env.LINKEDIN_ID || 'APP_ID',
-		clientSecret: process.env.LINKEDIN_SECRET || 'APP_SECRET',
-		callbackURL: 'http://localhost:3000/auth/linkedin/callback'
-	}
+    db: 'mongodb://localhost/audiofind-dev',
+    app: {
+        title: 'AudioFind - Development Environment'
+    },
+    facebook: {
+        clientID: '760760133944628',
+        clientSecret: 'b109ffa998a31f9a882f25b672b6eed8',
+        callbackURL: 'http://audiofind.local.com/auth/facebook/callback'
+    },
+    twitter: {
+        clientID: process.env.TWITTER_KEY || 'CONSUMER_KEY',
+        clientSecret: process.env.TWITTER_SECRET || 'CONSUMER_SECRET',
+        callbackURL: 'http://localhost:3000/auth/twitter/callback'
+    },
+    google: {
+        clientID: process.env.GOOGLE_ID || 'APP_ID',
+        clientSecret: process.env.GOOGLE_SECRET || 'APP_SECRET',
+        callbackURL: 'http://localhost:3000/auth/google/callback'
+    },
+    linkedin: {
+        clientID: process.env.LINKEDIN_ID || 'APP_ID',
+        clientSecret: process.env.LINKEDIN_SECRET || 'APP_SECRET',
+        callbackURL: 'http://localhost:3000/auth/linkedin/callback'
+    }
 };
